88d852fd6c705284805518fdce3e9e0b62c434ac
Sajabai <sudhanarayan28@gmail.com>
2021-12-15 11:43:31 +0530

    sudha

---
+++ src/test/java/GitBash/SampleTest.java
@@ -0,0 +1,14 @@
+package GitBash;
+
+public class SampleTest {
+
+	public static void main(String[] args) {
+int s1=10;
+int s2=20;
+
+int sum=s1+s2;
+
+System.out.println("the sum of number is"+sum);
+	}
+
+}
